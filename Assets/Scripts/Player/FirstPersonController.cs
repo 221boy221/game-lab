eff7b50c0860dfbbfcf4fd047f7e0da032251c4b
Boy Voesten <warrock.boy@hotmail.com>
2015-04-28 17:27:16 +0200

    Added raycast / object pickup prototype

--- Assets/Scripts/Player/FirstPersonController.cs
+++ Assets/Scripts/Player/FirstPersonController.cs
@@ -2,52 +2,52 @@
 using System.Collections;
 
 [RequireComponent(typeof(CharacterController))]
-public class FirstPersonController : MonoBehaviour {
+public class FirstPersonController : MonoBehaviour 
+{
 
     public float movementSpeed = 5.0f;
     public float mouseSensitivity = 5.0f;
     public float jumpSpeed = 20.0f;
-
-    float verticalRotation = 0;
     public float upDownRange = 60.0f;
-
-    float verticalVelocity = 0;
+    
+    private float _verticalRotation = 0;
+    private float _verticalVelocity = 0;
 
     CharacterController characterController;
 
-    void Start() {
-        Screen.lockCursor = true;   //(Doesn't work anymore since unity 4.6+)
+    void Start()
+    {
+        Screen.lockCursor = true;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         characterController = GetComponent<CharacterController>();
     }
 
-    void Update() {
-
+    void Update()
+    {
         // Rotation
         float rotLeftRight = Input.GetAxis("Mouse X") * mouseSensitivity;
         transform.Rotate(0, rotLeftRight, 0);
 
-
-        verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
-        verticalRotation = Mathf.Clamp(verticalRotation, -upDownRange, upDownRange);
-        Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+        _verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        _verticalRotation = Mathf.Clamp(_verticalRotation, -upDownRange, upDownRange);
+        Camera.main.transform.localRotation = Quaternion.Euler(_verticalRotation, 0, 0);
 
         // Movement
         float forwardSpeed = Input.GetAxis("Vertical") * movementSpeed;
         float sideSpeed = Input.GetAxis("Horizontal") * movementSpeed;
 
-        verticalVelocity += Physics.gravity.y * Time.deltaTime;
+        _verticalVelocity += Physics.gravity.y * Time.deltaTime;
 
-        if (characterController.isGrounded && Input.GetButton("Jump")) {
-            verticalVelocity = jumpSpeed;
+        if (characterController.isGrounded && Input.GetButton("Jump")) 
+        {
+            _verticalVelocity = jumpSpeed;
         }
 
-        Vector3 speed = new Vector3(sideSpeed, verticalVelocity, forwardSpeed);
-
+        Vector3 speed = new Vector3(sideSpeed, _verticalVelocity, forwardSpeed);
         speed = transform.rotation * speed;
-
 
         characterController.Move(speed * Time.deltaTime);
     }
+
 }
---
+++ Assets/Scripts/Player/ObjectInteraction.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectInteraction : MonoBehaviour 
+{
+
+    private GameObject col;
+
+    void OnTriggerEnter(Collider other)
+    {
+        col = other.gameObject;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        col = null;
+    }
+
+    void Update() 
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (col != null) // This way the RayCast will only run if a said object is in the collider
+            {
+                if (GrabAble())
+                {
+                    Debug.Log("True");
+                    Destroy(col.gameObject);
+                }
+                Debug.Log("False");
+            }
+        }
+    }
+
+    // Is the player aiming at the so called Object?
+    bool GrabAble() 
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Ray ray = new Ray(transform.position, forward);
+        RaycastHit hit;
+
+        Physics.Raycast(ray, out hit, 10f);
+        Debug.DrawRay(transform.position, forward);
+
+        // Return bool
+        return hit.collider.gameObject == col;
+    }
+
+}
