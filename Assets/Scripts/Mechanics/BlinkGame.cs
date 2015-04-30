c7be8ab6d3d0ac218b9b84e01cce93a28d5ea75e
rob verhoef <16617@ma-web.nl>
2015-04-30 10:00:17 +0200

    Worked on Blink Game

---
+++ Assets/Scripts/Mechanics/BlinkGame.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlinkGame : MonoBehaviour 
+{
+	[SerializeField] private GameObject _eyesOpen;
+	[SerializeField] private GameObject _eyesShut;
+	private float _waitTime;
+	[SerializeField]private bool _playerWin;
+	[SerializeField]private bool _pcWin;
+	
+	void Awake () 
+	{
+		_waitTime = Random.Range (15,90);
+		StartCoroutine (Blink());
+	}
+
+	IEnumerator Blink () 
+	{
+		yield return new WaitForSeconds (_waitTime);
+		_eyesOpen.SetActive(false);
+		if(_pcWin == false)
+		{
+			_playerWin = true;
+		}
+	}
+
+	void Update ()
+	{
+		if(_playerWin == true)
+		{
+			Debug.Log("Player Wins");
+		}
+		else if (_pcWin == true)
+		{
+			Debug.Log("PC Wins");
+		}
+	}
+
+	public void Button ()
+	{
+		if(_playerWin == false)
+		{
+			_pcWin = true;
+		}
+	}
+}
