266387c19f9fe9b4a9f80ebfd3ff021c00d5ddf0
221boy221 <warrock.boy@hotmail.com>
2015-05-11 00:07:09 +0200

    Added models, Fixed array index error of respawnscript, Added new respawnpoints, Added collision, adjusted player's height

--- Assets/Scripts/Objects/ObjBehaviour.cs
+++ Assets/Scripts/Objects/ObjBehaviour.cs
@@ -10,6 +10,7 @@ public class ObjBehaviour : MonoBehaviour
     void Start() 
     {
         _objRespawner = GameObject.FindGameObjectWithTag("ObjRespawner").GetComponent<ObjRespawner>();
+        _playerData = GameObject.FindGameObjectWithTag("GameController").GetComponent<PlayerData>();
     }
 
     public void OnInteraction() 
--- Assets/Scripts/Objects/ObjRespawner.cs
+++ Assets/Scripts/Objects/ObjRespawner.cs
@@ -9,13 +9,13 @@ public class ObjRespawner : MonoBehaviour
 
     public void Respawn(GameObject obj) 
     {
-        int i = Mathf.RoundToInt(Random.Range(1f, _spawnpoints.Length));
+        int i = Mathf.RoundToInt(Random.Range(0f, _spawnpoints.Length -1));
         GameObject spawnpoint = _spawnpoints[i];
 
         // So that it doesnt respawn on it's current pos
         if (obj.transform.position == spawnpoint.transform.position) 
         {
-            i = Mathf.RoundToInt(Random.Range(1f, _spawnpoints.Length));
+            i = Mathf.RoundToInt(Random.Range(0f, _spawnpoints.Length -1));
             spawnpoint = _spawnpoints[i];
         }
 
