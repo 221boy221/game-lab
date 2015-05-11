fff6b116ea030cdb08575f711aad2f33eac9988f
Boy Voesten <warrock.boy@hotmail.com>
2015-05-11 10:24:31 +0200

    Balanced player, improved flashlight, added score and time counters, adjusted UI canvas.

--- Assets/Scripts/Player/IngameUI.cs
+++ Assets/Scripts/Player/IngameUI.cs
@@ -2,18 +2,22 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class IngameUI : MonoBehaviour {
+public class IngameUI : MonoBehaviour 
+{
 
     [SerializeField] private Text _scoreTxt;
+    [SerializeField] private Text _timeTxt;
     private PlayerData _playerData;
 
-    void Start() {
+    void Start() 
+    {
         _playerData = GameObject.FindGameObjectWithTag("GameController").GetComponent<PlayerData>();
     }
 
     public void UpdateUI() 
     {
-        _scoreTxt.text = "Score " + _playerData.score;
+        _scoreTxt.text = "Score: " + _playerData.score;
+        _timeTxt.text = "Time: " + _playerData.time;
     }
 
 }
--- Assets/Scripts/Player/PlayerData.cs
+++ Assets/Scripts/Player/PlayerData.cs
@@ -1,19 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class PlayerData : MonoBehaviour 
 {
 
+    private float _time = 60;
     private int _score = 0;
     private IngameUI _ingameUI;
 
-    void Start() {
+    void Start() 
+    {
         _ingameUI = GameObject.FindGameObjectWithTag("IngameUI").GetComponent<IngameUI>();
+    }
+
+    void FixedUpdate() 
+    {
+        _time -= Time.deltaTime;
+        _time = (float)Math.Round(_time, 2);
+        _ingameUI.UpdateUI();
     }
 
     public void Increase(int amount) 
     {
-        _score += amount;
+        _time += amount;
+        _score += 1;
         _ingameUI.UpdateUI();
     }
 
@@ -25,6 +36,14 @@ public class PlayerData : MonoBehaviour
         get 
         {
             return _score;
+        }
+    }
+
+    public float time 
+    {
+        get 
+        {
+            return _time;
         }
     }
 
